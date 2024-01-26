5044811f7804e7cf83923908d1b35322f34fb3fc
Adam Stephens <adam@valkor.net>
2024-01-26 10:15:00 -0500

    cleanup(module): stop customizing kernel configf

--- packages/default.nix
+++ packages/default.nix
@@ -1,33 +1,6 @@
 { lib, pkgs, ... }:
 let
   sources = import ../npins;
-  kp = [
-    {
-      name = "x13s-cfg";
-      patch = null;
-      extraStructuredConfig = with lib.kernel; {
-        EFI_ARMSTUB_DTB_LOADER = lib.mkForce yes;
-        OF_OVERLAY = lib.mkForce yes;
-        BTRFS_FS = lib.mkForce yes;
-        BTRFS_FS_POSIX_ACL = lib.mkForce yes;
-        MEDIA_CONTROLLER = lib.mkForce yes;
-        SND_USB_AUDIO_USE_MEDIA_CONTROLLER = lib.mkForce yes;
-        SND_USB = lib.mkForce yes;
-        SND_USB_AUDIO = lib.mkForce module;
-        USB_XHCI_PCI = lib.mkForce module;
-        NO_HZ_FULL = lib.mkForce yes;
-        HZ_100 = lib.mkForce yes;
-        HZ_250 = lib.mkForce no;
-        DRM_AMDGPU = lib.mkForce no;
-        DRM_NOUVEAU = lib.mkForce no;
-        QCOM_TSENS = lib.mkForce yes;
-        NVMEM_QCOM_QFPROM = lib.mkForce yes;
-        ARM_QCOM_CPUFREQ_NVMEM = lib.mkForce yes;
-        VIRTIO_PCI = lib.mkForce module;
-        # forthcoming kernel work: QCOM_PD_MAPPER = lib.mkForce module;
-      };
-    }
-  ];
 
   linux_x13s_pkg =
     { buildLinux, ... }@args:
@@ -42,10 +15,9 @@ let
 
         src = sources.jhovold-linux;
 
-        kernelPatches = (args.kernelPatches or [ ]) ++ kp;
+        kernelPatches = args.kernelPatches or [ ];
         extraMeta.branch = lib.versions.majorMinor version;
       }
-      // (args.argsOverride or { })
     );
 in
 rec {
