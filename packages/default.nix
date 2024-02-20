b05262f8820597b05a023ead500846471fdf864d
Adam Stephens <adam@valkor.net>
2024-02-20 07:39:42 -0500

    remove shmbridge patch, unstable url

--- packages/default.nix
+++ packages/default.nix
@@ -16,19 +16,19 @@ let
         src = sources.linux;
 
         kernelPatches = (args.kernelPatches or [ ]) ++ [
-          {
-            # fix resets when reading EFI vars
-            name = "qcom-shm-bridge-tz";
-            patch = (
-              pkgs.fetchurl {
-                url = "https://lore.kernel.org/lkml/20240205182810.58382-1-brgl@bgdev.pl/t.mbox.gz";
-                hash = "sha256-kplvgUGE70eVivaN59Ozj7/utgnPEkVUjC8nemyM4vU=";
-              }
-            );
-            extraStructuredConfig = {
-              QCOM_TZMEM_MODE_SHMBRIDGE = lib.kernel.yes;
-            };
-          }
+          # {
+          #   # fix resets when reading EFI vars
+          #   name = "qcom-shm-bridge-tz";
+          #   patch = (
+          #     pkgs.fetchurl {
+          #       url = "https://lore.kernel.org/lkml/20240205182810.58382-1-brgl@bgdev.pl/t.mbox.gz";
+          #       hash = "sha256-MeuDkVFSf/KqzcD92wR6U9yOJhOKmZkXsgGcOm/9l/k=";
+          #     }
+          #   );
+          #   extraStructuredConfig = {
+          #     QCOM_TZMEM_MODE_SHMBRIDGE = lib.kernel.yes;
+          #   };
+          # }
         ];
         extraMeta.branch = lib.versions.majorMinor version;
       }
