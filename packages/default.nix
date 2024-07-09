07c7d73a57a3243cfacd11ffc97b16ea99a141b7
Adam Stephens <adam@valkor.net>
2024-07-09 14:23:37 -0400

    jhovold: 6.10.0-rc6 -> 6.10.0-rc7

--- packages/default.nix
+++ packages/default.nix
@@ -17,7 +17,7 @@ in
 rec {
   linux_jhovold = pkgs.callPackage linux_x13s_pkg {
     src = sources.linux-jhovold;
-    version = "6.10.0-rc6";
+    version = "6.10.0-rc7";
     defconfig = "johan_defconfig";
   };
 
