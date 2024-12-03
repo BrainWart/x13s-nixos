8a4f5712ce76357c0a56711cc8b55cdeae59e4a2
Cody McGinnis <cody@mcginnis.dev>
2024-12-03 00:35:50 +0000

    example config builds now

--- configurations/example.nix
+++ configurations/example.nix
@@ -1,20 +1,14 @@
-{ ... }:
 {
-  system = "aarch64-linux";
-  modules = [
-    "${builtins.fetchTarball "https://github.com/brainwart/x13s-nixos/archive/aef66c462abe817e33aad91d97aa782a1e2ad2c7.zip"}/module.nix"
-    (
-      { pkgs, ... }:
-      {
-        nixos-x13s.enable = true;
-        nixos-x13s.kernel = pkgs.x13s.linux_jhovold; # jhovold is default, but mainline supported
-
-        # allow unfree firmware
-        nixpkgs.config.allowUnfree = true;
-
-        # define your fileSystems
-        fileSystems."/".device = "/dev/notreal";
-      }
-    )
+  ...
+}:
+{
+  imports = [
+    ../module.nix
   ];
+
+  nixos-x13s.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "24.11";
 }
