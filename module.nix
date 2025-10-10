b54640293d8185d1212631d433f16e83839eacf0
Beaver <beavernotacat@gmail.com>
2025-10-10 06:14:03 +0300

    Rename networkmanager to modemmanager (#10)
    Nixos module rename causes
    ```
    evaluation warning: The option `networking.networkmanager.fccUnlockScripts' defined in `...' has been renamed to `networking.modemmanager.fccUnlockScripts'.
    ```

--- module.nix
+++ module.nix
@@ -127,7 +127,7 @@ in
     };
 
     # https://github.com/jhovold/linux/wiki/X13s#modem
-    networking.networkmanager.fccUnlockScripts = [
+    networking.modemmanager.fccUnlockScripts = [
       {
         id = "105b:e0c3";
         path = "${pkgs.modemmanager}/share/ModemManager/fcc-unlock.available.d/105b";
