cf295237b6d6e9cba28b49400b0d8710ccd56961
Cody McGinnis <cody@mcginnis.dev>
2026-01-30 22:25:16 +0000

    pkgs use reference from flake.lock

--- shell.nix
+++ shell.nix
@@ -1,5 +1,15 @@
 {
-  pkgs ? (import <nixpkgs> { }),
+  pkgs ? (
+    let
+      flakeLock = (builtins.fromJSON (builtins.readFile ./flake.lock));
+      source =
+        with flakeLock.nodes.nixpkgs.locked;
+        fetchTarball {
+          url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        };
+    in
+    import source { }
+  ),
 }:
 pkgs.mkShellNoCC {
   packages = [
